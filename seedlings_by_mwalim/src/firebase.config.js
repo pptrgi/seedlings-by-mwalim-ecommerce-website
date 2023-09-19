ddf284ffeaa5d55f4f9104fb1d1d1c128eac3403
pptrgi <pptrgitonga@gmail.com>
2023-09-19 16:14:36 +0300

    Access keys as env variables

--- seedlings_by_mwalim/src/firebase.config.js
+++ seedlings_by_mwalim/src/firebase.config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 
 // The web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBFF-GiVDwL-PpmXoQyncze3Tve1lhzVoA",
-    authDomain: "seedlingsbymwalim.firebaseapp.com",
-    projectId: "seedlingsbymwalim",
-    storageBucket: "seedlingsbymwalim.appspot.com",
-    messagingSenderId: "1039226264985",
-    appId: "1:1039226264985:web:f093609fc68ad92a6a0708"
-  };
+  apiKey: `${import.meta.env.VITE_FBASE_APIKEY}`,
+  authDomain: `${import.meta.env.VITE_FBASE_AUTH_DOMAIN}`,
+  projectId: "seedlingsbymwalim",
+  storageBucket: `${import.meta.env.VITE_FBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${import.meta.env.VITE_FBASE_MESSAGING_SENDER_ID}`,
+  appId: `${import.meta.env.VITE_FBASE_APP_ID}`,
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
