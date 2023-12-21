1a031c1a06365541c37d69866dad106c26ff644d
pptrgi <pptrgitonga@gmail.com>
2023-12-21 17:16:52 +0300

    Add cors options

---
+++ backend_server/utils/corsOptions.js
@@ -0,0 +1,17 @@
+const allowedOrigins = [
+  "https://seedlingsbymwalim.vercel.app",
+  "http://localhost:5173",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
